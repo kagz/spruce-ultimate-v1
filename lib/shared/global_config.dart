53d497e088850a80727e6901fb381622d518f5e9
kagz <kagwiandrew@gmail.com>
2019-11-07 17:02:52 +0300

    Initial commit

---
+++ lib/shared/global_config.dart
@@ -0,0 +1,5 @@
+final String API_KEY = 'AIzaSyCa2VuBK_54uSIygqegmguYVJiaCk9gliU'; //mapview key
+final String AUTH_END_POINT_URL = 'https://identitytoolkit.googleapis.com/v1';
+final String FB_KEY = 'AIzaSyCa2VuBK_54uSIygqegmguYVJiaCk9gliU';
+final String CRUD_ENDPOINT =
+    'https://kamagera-aa372.firebaseio.com'; //https://kamagera-aa372.firebaseio.com/
