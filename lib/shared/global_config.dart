addf184ab9200989351c7db86186ed557601b093
kagz <kagwiandrew@gmail.com>
2019-11-07 19:48:04 +0300

    ultimate working

--- lib/shared/global_config.dart
+++ lib/shared/global_config.dart
@@ -1,4 +1,4 @@
-final String API_KEY = 'AIzaSyCa2VuBK_54uSIygqegmguYVJiaCk9gliU'; //mapview key
+final String API_KEY = 'AIzaSyDWvg27aD4Nev_IyRTOdW8cGXleYGA17I0'; //mapview key
 final String AUTH_END_POINT_URL = 'https://identitytoolkit.googleapis.com/v1';
 final String FB_KEY = 'AIzaSyCa2VuBK_54uSIygqegmguYVJiaCk9gliU';
 final String CRUD_ENDPOINT =
